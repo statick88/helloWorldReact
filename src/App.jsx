54a6b77d713ebb74e5435fca7fe647fe58f0314c
Diego Saavedra <dsaavedra88@gmail.com>
2024-04-03 21:48:18 -0500

    hello world react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit impedit sed minima quisquam quam, reiciendis rem, repellat saepe magnam dolores iste sapiente totam officia? Maiores vel architecto fuga! Eius, ut?</p>
+      <hr />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum maxime voluptatem necessitatibus aliquam, nam ipsum, quisquam iste non temporibus nisi, quis nihil rem repudiandae eligendi architecto ipsa cupiditate cumque id.</p>
+    </>
+  )
+}
+
+export default App
